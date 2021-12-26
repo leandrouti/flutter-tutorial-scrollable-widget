29b525c90d9cc820d772d1a611ac083309cbd817
Leandro T. Utiyama <lutiyama@gmail.com>
2021-12-26 13:07:57 +0900

    [add] Challenge 1 scroll view controller

--- lib/screens/explore_screen.dart
+++ lib/screens/explore_screen.dart
@@ -4,10 +4,44 @@ import '../components/components.dart';
 import '../models/models.dart';
 import '../api/mock_fooderlich_service.dart';
 
-class ExploreScreen extends StatelessWidget {
+class ExploreScreen extends StatefulWidget {
+  const ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  State<ExploreScreen> createState() => _ExploreScreenState();
+}
+
+class _ExploreScreenState extends State<ExploreScreen> {
   final mockService = MockFooderlichService();
 
-  ExploreScreen({Key? key}) : super(key: key);
+  late ScrollController _scrollController;
+
+  void _scrollListener() {
+    if (_scrollController.offset >=
+            _scrollController.position.maxScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('i am at the bottom!');
+    }
+
+    if (_scrollController.offset <=
+            _scrollController.position.minScrollExtent &&
+        !_scrollController.position.outOfRange) {
+      print('i am at the top!');
+    }
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.removeListener(_scrollListener);
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +50,7 @@ class ExploreScreen extends StatelessWidget {
         builder: (context, AsyncSnapshot<ExploreData> snapshot) {
           if (snapshot.connectionState == ConnectionState.done) {
             return ListView(
+              controller: _scrollController,
               scrollDirection: Axis.vertical,
               children: [
                 TodayRecipeListView(recipes: snapshot.data?.todayRecipes ?? []),
